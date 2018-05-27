2615f978b7545c8600693e4bf622eb5fe051ecb2
Travis Roberts <travis.roberts2@gmail.com>
2018-05-27 15:06:56 -0500

    trying anything and everything

--- test/e2e/amazonBookSearch.js
+++ test/e2e/amazonBookSearch.js
@@ -1,52 +1,125 @@
 var cheerio = require("cheerio");
 var category = 'books';
+const linkList = [];
+
 
 module.exports = {
 
-  'Amazon Book Scraper': function(client) {
-    let ids = [];
+  before: function(client) {
     client
       .url('http://amazon.com')
       .waitForElementVisible('body')
       .assert.title('Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more')
       .setValue('#twotabsearchtextbox', category)
       .click('.nav-input')
+  },
+
+  // beforeEach: function(client) {
+  //   client
+  //   .elements('css selector', '.aok-align-center', function(links) {
+  //     for (var i = 0; i < links.value.length; i++) {
+  //       ids.push(links.value[i].ELEMENT);
+  //     }
+  //   })
+  // },
+  'Gather links': function(client) {
+    client
       .elements('css selector', '.aok-align-center', function(links) {
-        // for (let i = 0; i < links.value.length; i++) {
-        //   console.log(links.value[i])
-        //   client.elementIdClick(links.value[i].ELEMENT, function() {
-        //     console.log(i)
-        //     client.back();
-        //   });
-        //
-        //   client.pause(1000);
-        // }
-        for (var i = 0; i < links.value.length; i++) {
-          ids.push(links.value[i].ELEMENT);
-          ids.forEach(function(link, j) {
-            client.pause(3000)
-            .elementIdClick(link, function() {
-              client.back()
+        for (var i = 0; i <links.value.length; i++) {
+          linkList.push(links.value[i].ELEMENT)
+          done()
+        })
+      })
+  }
+  'Amazon Book Scraper': function(client) {
+    var i = 0;
+
+    client
+      .elements('css selector', '.aok-align-center', function(links) {
+
+      function iterate(cb) {
+        cb();
+      }
+        for (var i = 0; i < links.value.length) {
+
+
+            iterate(function() {
+              client
+              .elementIdClick(links.value[i].ELEMENT)
+              .waitForElementVisible('body')
             })
-          })
+            client.back(function() {
+              i++;
+            })
         }
 
-        // for (let j = 0; j < 5; j++) {
-        // (function(cntr) {
-        //   client.elementIdClick(ids[j], function() {
-        //     client.back()
-        //     client.pause(3000)
-        //     console.log(ids[j])
-        //   })
-        // })
-        //
-        //
-        // }
-        console.log(links.value)
+
       })
-      .end()
+        // for (var i = 0; i < result1.value.length; i++) {
+        //   client
+        //     .elementIdClick(result1.value[i].ELEMENT, function(print) {
+        //
+        //     });
+        //     console.log(i)
+        //     client.back();
+        //     client.waitForElementVisible('body')
+        // }
 
 
+    // ids.forEach(function(link, i) {
+    //   setInterval(scrape.bind(null, link), 2000);
+    // });
   }
 
-}
+  //
+  // 'Amazon Book Scraper': function(client) {
+  //
+  //
+  //  const ids = [];
+  //   client
+  //     .url('http://amazon.com')
+  //     .waitForElementVisible('body')
+  //     .assert.title('Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more')
+  //     .setValue('#twotabsearchtextbox', category)
+  //     .click('.nav-input')
+  //     .elements('css selector', '.aok-align-center',  function(links) {
+  //       for (var i = 0; i < links.value.length; i++) {
+  //         // ids.push(links.value[i].ELEMENT);
+  //         let link = links.value[i].ELEMENT;
+  //         setTimeout(function(link) {
+  //           scrape(link);
+  //         }, 2000)
+  //       }
+        // ids.forEach(function(link, j) {
+        //   client
+        //   .elementIdClick(link, (function() {
+        //     client
+        //     .back()
+        //     .pause(1000)
+        //   })())
+        //   })
+        // console.log(ids)
+        // for (var i = 0; i < ids.length; i++) {
+        //   link = ids[i]
+        //   scrape(link);
+        // }
+      //   ids.forEach(function(link, j) {
+      //     scrape(link)
+      //     console.log(j)
+      // })
+
+        // client
+        //   .elementIdClick(link)
+        //   .pause(1000)
+        //   .back()
+      // })
+      // .back(function() {
+      //   ids.forEach(function(link, j) {
+      //       client
+      //         .elementIdClick(link)
+      //         .pause(1000)
+      //   })
+      // })
+      // client.end()
+    // }
+  }
